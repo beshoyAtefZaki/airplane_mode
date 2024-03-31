2f3e49babcdffeaf16ab34bfe93965b132653529
Beshoy <beshoyatef31@gmail.com>
2024-03-31 02:27:59 +0200

    add visit website link

--- airplane_mode/airplane_mode/doctype/airline/airline.js
+++ airplane_mode/airplane_mode/doctype/airline/airline.js
@@ -1,8 +1,11 @@
 // Copyright (c) 2023, beshoy.atef@dynamiceg.com and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Airline", {
-// 	refresh(frm) {
+frappe.ui.form.on("Airline", {
+	refresh(frm) {
 
-// 	},
-// });
+        if (frm.doc.website) {
+            frm.add_web_link(frm.doc.website , "Visit website")
+        }
+	},
+});
